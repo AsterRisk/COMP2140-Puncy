64b7e11e35a82268c1dd6437d22bb6cd452ba790
AsterRisk <31030090+AsterRisk@users.noreply.github.com>
2020-12-01 23:21:54 -0500

    Add files via upload

---
+++ COMP2140_Puncy/php_scripts/header.php
@@ -0,0 +1,30 @@
+<?php
+    if(!isset($_SESSION))
+    {
+        
+        session_start();
+        //include 'login.php';
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        
+        $conn = new mysqli($servername, $username, $password);
+        if ($conn->connect_error)
+        {
+            die("Connection to database failed: " . $conn->connect_error);
+        }
+        else
+        {
+            //echo $_SESSION['id'];
+            $sql = "use puncys_store;";
+            $conn->prepare($sql);
+            $conn->query($sql);
+        }
+    }
+    else
+    {
+            
+            
+        
+    }
+?>
